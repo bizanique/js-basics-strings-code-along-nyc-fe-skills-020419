5f01860e8a0435db56ab12d15047403283a26ffa
shaniqua simpson <niquacharisdesigns@gmail.com>
2019-03-07 01:46:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ const currentUser = 'Grace Hopper';
 
 const welcomeMessage = 'Welcome to Flatbook, Grace Hopper!';
 const upperCaseCurrentUser = currentUser.toUpperCase();
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
